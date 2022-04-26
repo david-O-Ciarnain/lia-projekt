6851ca19864f92c859fa89927a7accc93c937b08
olga <oklimovich37@gmail.com>
2022-04-26 15:22:21 +0200

    Co-authored-by: AnPe92 <AnPe92@users.noreply.github.com> Co-authored-by: Konstantin Vasiliadis <cocce@graffiti.net> Co-authored-by: david-O-Ciarnain <david-O-Ciarnain@users.noreply.github.com> Co-authored-by: MikaelWilhelmsson <MikaelWilhelmsson@users.noreply.github.com> Co-authored-by: UffeMan <UffeMan@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button,View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      
+      <Text style={styles.header}>Cleaning Support</Text>
+     
+      
+      <Button style={styles.button}
+        title="Tengella"
+        color="#4682B4"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+<Button
+
+        title="Nyhetsflöde"
+        color="#4682B4"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+<Button
+        title="Mina sidor"
+        color="#4682B4"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+<Button
+        title="Förbrukningsmaterial"
+        color="#4682B4"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+<Button
+        title="Kalender"
+        color="#4682B4"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+<Button
+        title="Personalhandbok"
+        color="#4682B4"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+<Button
+        title="Blanketter vid olycka"
+        color="#4682B4"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+<Button
+        title="Arbetsmiljöhandbok"
+        color="#4682B4"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+     <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+    backgroundColor:'#fff',
+    
+    justifyContent:'space-around',
+    marginHorizontal:8,
+    
+    
+    
   },
+
+  header:{
+    color:'#00BFFF',
+    fontSize: 25,
+  
+  },
+
+  title: {
+    textAlign: 'center',
+    
+  },
+
+  button:{
+    
+    
+  }
 });
