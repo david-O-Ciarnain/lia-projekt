309afc554dae1744d7388cb61addbeba4ffa99c2
olga <oklimovich37@gmail.com>
2022-04-27 10:34:50 +0200

    Co-authored-by: UffeMan <UffeMan@users.noreply.github.com> Co-authored-by: MikaelWilhelmsson <MikaelWilhelmsson@users.noreply.github.com> Co-authored-by: Konstantin Vasiliadis <cocce@graffiti.net>

--- App.js
+++ App.js
@@ -1,62 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Button,View, SafeAreaView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  SafeAreaView,
+  Alert,
+  Image,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      
-      <Text style={styles.header}>Cleaning Support</Text>
-     
-      
-      <Button style={styles.button}
-        title="Tengella"
-        color="#4682B4"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      <Image source={require("./assets/CS_logo_vert.png")} />
 
-<Button
+      <View style={styles.buttonView}>
+        <Image style={styles.image} source={require("./assets/Tengella.png")} />
 
-        title="Nyhetsflöde"
-        color="#4682B4"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+        <Button
+          style={styles.button}
+          title="Tengella"
+          color="#4682B4"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
 
-<Button
-        title="Mina sidor"
-        color="#4682B4"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      <View style={styles.buttonView}>
+        <Image style={styles.image} source={require("./assets/nyhet.png")} />
+        <Button
+          style={styles.button}
+          title="Nyhetsflöde"
+          color="#4682B4"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
 
-<Button
-        title="Förbrukningsmaterial"
-        color="#4682B4"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      <View style={styles.buttonView}>
+        <Image
+          style={styles.image}
+          source={require("./assets/mina_sidor.png")}
+        />
+        <Button
+          style={styles.button}
+          title="Mina sidor"
+          color="#4682B4"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
 
-<Button
-        title="Kalender"
-        color="#4682B4"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      <View style={styles.buttonView}>
+        <Image
+          style={styles.image}
+          source={require("./assets/spray_bottle.webp")}
+        />
+        <Button
+          style={styles.button}
+          title="Förbrukningsmaterial"
+          color="#4682B4"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
 
-<Button
-        title="Personalhandbok"
-        color="#4682B4"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      <View style={styles.buttonView}>
+        <Image style={styles.image} source={require("./assets/calender.png")} />
+        <Button
+          style={styles.button}
+          title="Kalender"
+          color="#4682B4"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
 
-<Button
-        title="Blanketter vid olycka"
-        color="#4682B4"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      <View style={styles.buttonView}>
+        <Image
+          style={styles.image}
+          source={require("./assets/personalhandbok.webp")}
+        />
+        <Button
+          style={styles.button}
+          title="Personalhandbok"
+          color="#4682B4"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
 
-<Button
-        title="Arbetsmiljöhandbok"
-        color="#4682B4"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-     <StatusBar style="auto" />
+      <View style={styles.buttonView}>
+        <Image
+          style={styles.image}
+          source={require("./assets/1510827013283693933093_negate.png")}
+        />
+        <Button
+          style={styles.button}
+          title="Blanketter vid olycka"
+          color="#4682B4"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
+
+      <View style={styles.buttonView}>
+        <Image
+          style={styles.image}
+          source={require("./assets/arbetsmiljöhandbok.png")}
+        />
+        <Button
+          style={styles.button}
+          title="Arbetsmiljöhandbok"
+          color="#4682B4"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
+
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -64,29 +118,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
-    backgroundColor:'#fff',
-    
-    justifyContent:'space-around',
-    marginHorizontal:8,
-    
-    
-    
+    backgroundColor: "#fff",
+
+    justifyContent: "space-around",
+
+    marginHorizontal: 8,
   },
 
-  header:{
-    color:'#00BFFF',
+  header: {
+    color: "#00BFFF",
     fontSize: 25,
-  
+  },
+
+  buttonView: {
+    backgroundColor: "#4682B4",
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+
+  image: {
+    width: 105,
+    height: 80,
   },
 
   title: {
-    textAlign: 'center',
-    
+    textAlign: "center",
   },
-
-  button:{
-    
-    
-  }
 });
