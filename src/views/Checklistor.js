fd879f9fee0f99d9999afcca513cf794e300ef65
AnPe92 <93088524+AnPe92@users.noreply.github.com>
2022-05-09 11:24:39 +0200

    Views

---
+++ src/views/Checklistor.js
@@ -0,0 +1,10 @@
+import { View, Image, Text } from 'react-native'
+
+export default function Checklistor() {
+    return (
+        <View>
+            <Image source={require("../../assets/arbetsmiljöhandbok.png")} />
+            <Text>Arbetsmiljöhandbok</Text>
+        </View>
+    )
+}
